4575996e06f5fa8050ed6207f103ce832aa10410
Михаил Серёгин <seregin.m.andr@sberbank.ru>
2020-08-07 12:29:03 +0300

    fix

--- src/shared/database/migrations/1596791141198-create-userToken-migration.ts
+++ src/shared/database/migrations/1596791141198-create-userToken-migration.ts
@@ -1,9 +1,32 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createUserTokenMigration1596791141198 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'UserToken',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+        },
+        {
+          name: 'expiresAt',
+          type: 'timestamp',
+        },
+        {
+          name: 'string',
+          type: 'varchar',
+        },
+        {
+          name: 'userID',
+          type: 'int',
+        },
+      ],
+    }), true, true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('UserToken');
   }
 }
