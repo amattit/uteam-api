e73899dea80c5672f9e65bb4f21b02060e603228
Михаил Серёгин <seregin.m.andr@sberbank.ru>
2020-08-07 12:28:35 +0300

    fix

--- src/shared/database/migrations/1596790559205-create-project-labelType-migration.ts
+++ src/shared/database/migrations/1596790559205-create-project-labelType-migration.ts
@@ -1,9 +1,28 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createProjectLabelTypeMigration1596790559205 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'ProjectLabelType_Relations',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+        },
+        {
+          name: 'prjcetId',
+          type: 'int',
+        },
+        {
+          name: 'labelId',
+          type: 'int',
+        },
+      ],
+    }), true, true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('ProjectLabelType_Relations');
   }
 }
--- src/shared/database/migrations/1596790961130-create-shareType-migration.ts
+++ src/shared/database/migrations/1596790961130-create-shareType-migration.ts
@@ -1,9 +1,24 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createShareTypeMigration1596790961130 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'ShareType',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+        },
+        {
+          name: 'title',
+          type: 'string',
+        },
+      ],
+    }), true, true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('ShareType');
   }
 }
